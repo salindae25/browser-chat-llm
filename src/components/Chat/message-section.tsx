43119f60436624a59d1c5c70bfade116575b2bbb
Salinda <salindae25@gmail.com>
2025-05-21 23:16:42 +0530

    feat(chat): add message regeneration and deletion functionality
    This commit introduces the ability to regenerate messages from a specific point in the chat and delete messages along with their subsequent messages. Additionally, it includes the addition of the @radix-ui/react-tabs dependency for future UI enhancements and updates the settings layout to use tabs for better organization.

--- src/components/Chat/message-section.tsx
+++ src/components/Chat/message-section.tsx
@@ -1,23 +1,73 @@
 import MessageLoading from "@/components/ui/message-loading";
-import { activeChatStore, messageStore } from "@/lib/chat-store";
+import { activeChatStore } from "@/lib/chat-store";
+import { db } from "@/lib/db";
+import { regenerateFromMessageIndex } from "@/lib/services";
 import { cn } from "@/lib/utils";
 import { useStore } from "@tanstack/react-store";
 import type { CoreMessage } from "ai";
+import { useLiveQuery } from "dexie-react-hooks";
+import { CopyIcon, RefreshCcwIcon, Trash2Icon } from "lucide-react";
 import { memo } from "react";
+import { toast } from "sonner";
+import { Button } from "../ui/button";
 import MarkdownRenderer from "./markdown-render";
 
 export const MessagesSection = memo(() => {
-	const messages = useStore(messageStore, (s) => s.messages);
+	const chatId = useStore(activeChatStore, (s) => s.chatId);
+	const messages = useLiveQuery(async () => {
+		const chatSession = await db.chatSessions.get(chatId);
+		return chatSession?.messages ?? [];
+	}, [chatId]);
+	const regenerateFromMessage = async (
+		isUserMessage: boolean,
+		messageIndex: number,
+	) => {
+		if (!isUserMessage) {
+			// TODO: implement forking
+			return;
+		}
+		await regenerateFromMessageIndex(messageIndex, chatId);
+	};
+	const deleteChatMessage = async (messageIndex: number) => {
+		await db.chatSessions.update(chatId, {
+			messages: messages?.slice(0, messageIndex),
+		});
+	};
+	const copyToClipboard = (text: string) => {
+		navigator.clipboard.writeText(text);
+		toast.info("Message copied to clipboard");
+	};
 	return (
 		<>
-			{messages.map((item, i) => {
+			{messages?.map((item, i) => {
 				// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-				return <MessageItem key={i} data={item} />;
+				return (
+					<MessageItem
+						key={`${chatId}-${i}`}
+						data={item}
+						messageIndex={i}
+						regenerateFromMessage={regenerateFromMessage}
+						deleteChatMessage={deleteChatMessage}
+						copyToClipboard={copyToClipboard}
+					/>
+				);
 			})}
 		</>
 	);
 });
-const MessageItem = ({ data }: { data: CoreMessage }) => {
+const MessageItem = ({
+	data,
+	messageIndex,
+	regenerateFromMessage,
+	deleteChatMessage,
+	copyToClipboard,
+}: {
+	data: CoreMessage;
+	messageIndex: number;
+	regenerateFromMessage: (isUserMessage: boolean, messageIndex: number) => void;
+	deleteChatMessage: (messageIndex: number) => void;
+	copyToClipboard: (text: string) => void;
+}) => {
 	const isUserMessage = data.role === "user";
 	return (
 		<div
@@ -27,7 +77,7 @@ const MessageItem = ({ data }: { data: CoreMessage }) => {
 			})}
 		>
 			<div
-				className={cn({
+				className={cn("relative", {
 					"max-w-[85%] flex-1 sm:max-w-[75%]": isUserMessage,
 					"max-w-full flex-1": !isUserMessage,
 				})}
@@ -44,6 +94,47 @@ const MessageItem = ({ data }: { data: CoreMessage }) => {
 						</MarkdownRenderer>
 					)}
 				</div>
+				<div
+					className={cn("relative flex gap-1 cursor-pointer", {
+						"-bottom-0 -left-1": isUserMessage,
+					})}
+				>
+					{isUserMessage && (
+						<>
+							<Button
+								aria-label="Delete message and following messages"
+								variant="noShadowNeutral"
+								className="size-8 border-none cursor-pointer"
+								size="icon"
+								onClick={() => deleteChatMessage(messageIndex)}
+							>
+								<Trash2Icon />
+							</Button>
+							<Button
+								aria-label="Regenerate message from this point"
+								variant="noShadowNeutral"
+								className="size-8 border-none cursor-pointer"
+								size="icon"
+								onClick={() =>
+									regenerateFromMessage(isUserMessage, messageIndex)
+								}
+							>
+								<RefreshCcwIcon />
+							</Button>
+						</>
+					)}
+					{!isUserMessage && (
+						<Button
+							aria-label="Copy message"
+							variant="noShadowNeutral"
+							className="size-8 border-none cursor-pointer"
+							size="icon"
+							onClick={() => copyToClipboard(data.content as string)}
+						>
+							<CopyIcon />
+						</Button>
+					)}
+				</div>
 			</div>
 		</div>
 	);
@@ -57,7 +148,20 @@ export const GeneratingMessage = memo(() => {
 		) : (
 			<MessageItem
 				key={-3}
+				messageIndex={-3}
 				data={{ role: "assistant", content: activeMessage }}
+				regenerateFromMessage={(
+					_isUserMessage: boolean,
+					_messageIndex: number,
+				): void => {
+					return;
+				}}
+				deleteChatMessage={(_messageIndex: number): void => {
+					return;
+				}}
+				copyToClipboard={(_text: string): void => {
+					return;
+				}}
 			/>
 		)
 	) : null;
