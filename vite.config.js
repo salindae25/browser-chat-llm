9367a5cc30035c6859b27edad0f8dd99d7030494
Salinda <salindae25@gmail.com>
2025-05-26 01:44:11 +0530

    chore: reduce the build chunk size

--- vite.config.js
+++ vite.config.js
@@ -15,17 +15,55 @@ export default defineConfig({
 		rollupOptions: {
 			output: {
 				manualChunks: {
-					ai_sdk: ['@ai-sdk/google', '@ai-sdk/openai', '@ai-sdk/openai-compatible', '@ai-sdk/groq'],
-					react: ['react', 'react-dom', '@ai-sdk/react',],
-					markdown:[ 'react-markdown'],
-					syntax_highlighter:[ 'react-syntax-highlighter'],
-					vendor: ['@radix-ui/react-avatar', '@radix-ui/react-collapsible', '@radix-ui/react-dialog',
-						'@radix-ui/react-dropdown-menu', '@radix-ui/react-label', '@radix-ui/react-select',
-						'@radix-ui/react-slot', '@radix-ui/react-switch', '@radix-ui/react-tooltip',
-						'lucide-react', 'sonner', 'date-fns', 'dexie']
-				}
-			}
-		}
+					ai_sdk: [
+						"@ai-sdk/google",
+						"@ai-sdk/openai",
+						"@ai-sdk/openai-compatible",
+						"@ai-sdk/groq",
+						"ai",
+					],
+					react: ["react", "react-dom", "@ai-sdk/react"],
+					markdown: ["react-markdown"],
+					syntax_highlighter: [
+						"react-syntax-highlighter",
+						"rehype-raw",
+						"rehype-katex",
+						"remark-math",
+						"remark-gfm",
+					],
+					other: [
+						"tailwindcss",
+						"@tanstack/react-query",
+						"@tanstack/react-router",
+						"@tanstack/react-store",
+						"@tanstack/store",
+						"@vercel/analytics",
+						"class-variance-authority",
+						"clsx",
+						"dexie-react-hooks",
+						"katex",
+						"tailwind-merge",
+						"tailwindcss-animate",
+						"use-stick-to-bottom",
+						"zod",
+					],
+					vendor: [
+						"@radix-ui/react-avatar",
+						"@radix-ui/react-collapsible",
+						"@radix-ui/react-dialog",
+						"@radix-ui/react-dropdown-menu",
+						"@radix-ui/react-label",
+						"@radix-ui/react-select",
+						"@radix-ui/react-slot",
+						"@radix-ui/react-switch",
+						"@radix-ui/react-tooltip",
+						"lucide-react",
+						"sonner",
+						"dexie",
+					],
+				},
+			},
+		},
 	},
 	resolve: {
 		alias: {
